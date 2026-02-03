1f90d4a038f6d06bf9e2abec6745a4fcb87783dd
COMPUS025\daw2 <frojob01@educantabria.es>
2026-02-03 18:14:13 +0100

    creación springboot

---
+++ garagebenz/src/main/java/com/garagebenz/GaragebenzApplication.java
@@ -0,0 +1,13 @@
+package com.garagebenz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaragebenzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GaragebenzApplication.class, args);
+	}
+
+}
---
+++ garagebenz/src/test/java/com/garagebenz/GaragebenzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.garagebenz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GaragebenzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
