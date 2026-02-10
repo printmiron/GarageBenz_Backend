7333674582730427b603411e9c4ea18ac071e492
COMPUS025\daw2 <frojob01@educantabria.es>
2026-02-10 17:06:39 +0100

    Cita modificado

--- demo/src/main/java/com/garagebenz/demo/repository/CitaRepository.java
+++ demo/src/main/java/com/garagebenz/demo/repository/CitaRepository.java
@@ -1,5 +1,6 @@
 package com.garagebenz.demo.repository;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,5 +10,9 @@ import com.garagebenz.demo.models.Cita;
 
 @Repository
 public interface CitaRepository extends JpaRepository<Cita, UUID> {
+    
+    List<Cita> findByClienteId(UUID idCliente);
+
+    List<Cita> findByEstado(Cita.EstadoCita estado);
     
 }
